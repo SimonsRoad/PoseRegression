16020ba70486a764c58569ee36aaed865c3141e4
Namhoon Lee <namhoonlee09@gmail.com>
2016-02-11 02:47:00 -0500

    old backup

--- src/script_poseregression.lua
+++ src/script_poseregression.lua
@@ -1,12 +1,14 @@
 --[[
---script_poseregression.lua
+--script_poseregression_new.lua
 --Namhoon Lee, The Robotics Institute, Carnegie Mellon University
 --]]
 
 
--- Dependencies
 local matio = require 'matio'
 require 'optim'
+require 'cudnn'
+require 'cunn';
+paths.dofile('util.lua')
 paths.dofile('datafromlist.lua')
 paths.dofile('create_network.lua')
 paths.dofile('compute_distance.lua')
@@ -14,19 +16,13 @@ paths.dofile('save_results.lua')
 paths.dofile('misc_utils.lua')
 
 
--- 0. settings
-task = 'PoseRegression'
---part = 'upperbody'   -- nJoints:8, modelNumber:7
---part = 'lowerbody'   -- nJoints:6, modelNumber:8
-part = 'fullbody'   -- nJoints:14, modelNumber:9
-nJoints = 14
-modelNumber = 9
+-- 0. settings + loading
+paths.dofile('load_settings.lua')
+print(string.format('\n**Performing [%s] modelNumber: %d\n', opt.t, modelNumber))
 
-nPoolSize = 12000
+nPoolSize = 13344
 nTrainData = 10000
 nTestData = 2000
-
-time = get_time()
 
 
 -- 1. load and normalize data
@@ -37,27 +33,21 @@ idx_pool = torch.randperm(nPoolSize)
 idx_train = idx_pool:narrow(1,1,nTrainData)
 idx_test = idx_pool:narrow(1,nTrainData+1,nTestData)
 
-trainset_sample = mydataloader:get_randomly_indices(idx_train)
+trainset_data = mydataloader:get_randomly_indices(idx_train)
 trainset_label = mydataloader:get_label(part, idx_train)
-trainset = {data = trainset_sample, label = trainset_label} 
-print (trainset)
-
-testset_sample = mydataloader:get_randomly_indices(idx_test)
+trainset = {data = trainset_data, label = trainset_label} 
+testset_data = mydataloader:get_randomly_indices(idx_test)
 testset_label = mydataloader:get_label(part, idx_test)
-testset = {data = testset_sample, label = testset_label}
-print (testset)
+testset = {data = testset_data, label = testset_label}
 
-save_testdata(testset, part, 'testpurpose') 	-- save into mat file
-
-assert(testset.label:size(1) == nTestData)
-assert(testset.label:size(2) == nJoints*2)
+--print (trainset); print (testset)
+assert(testset.label:size(1) == nTestData);assert(testset.label:size(2) == nJoints*2)
 
 setmetatable(trainset,
 {__index = function(t,i)
 	return {t.data[i], t.label[i]}
 end}
 );
-
 function trainset:size()
 	return self.data:size(1)
 end
@@ -73,13 +63,15 @@ for i=1,3 do
 	trainset.data[{ {}, {i}, {}, {} }]:div(stdv[i])
 end
 
+-- save testset into .mat file for visualization 
+print('Saving everything to: ' .. opt.save)
+os.execute('mkdir -p ' .. opt.save)
+matio.save(paths.concat(opt.save,string.format('testdata_%s.mat', opt.t)), testset)
+
 
 -- 2. network
 --
 model = create_network(modelNumber)
-
--- cudnn
-require 'cudnn'
 cudnn.convert(model, cudnn)
 
 
@@ -87,11 +79,7 @@ cudnn.convert(model, cudnn)
 -- 
 criterion = nn.MSECriterion()
 
-
--- 4. trian the network
---
--- cuda setup
-require 'cunn';
+-- *change to cuda 
 model = model:cuda()
 criterion = criterion:cuda()
 trainset.data = trainset.data:cuda()
@@ -99,19 +87,25 @@ trainset.label = trainset.label:cuda()
 testset.data = testset.data:cuda()
 testset.label = testset.label:cuda()
 
--- trainer
-trainer = nn.StochasticGradient(model, criterion)
-trainer.maxIteration = 100
-trainer.learningRate = 0.005
-trainer.learningRateDecay = 0.1
---trainer.weightDecay = 0.0005
---trainer.momentum = 0.9
+-- *Optional
+cutorch.setDevice(opt.GPU)
+print(opt)
 
--- START TRAINING
-print("maxIteration: " .. trainer.maxIteration)
-print("learningRate: " .. trainer.learningRate)
-print("learningRateDecay: " .. trainer.learningRateDecay)
-trainer:train(trainset)
+
+-- 4. (NEW) TRAINING  
+--
+TRAINING = false
+if TRAINING then
+	paths.dofile('train.lua')
+	epoch = opt.epochNumber
+	for i=1, opt.nEpochs do
+		train()
+		epoch = epoch + 1
+	end
+else
+	-- *load existing model
+	model = torch.load(modelSaved)
+end
 
 
 -- 5. test the network
@@ -121,31 +115,23 @@ for i=1,3 do
 	testset.data[{ {}, {i}, {}, {} }]:div(stdv[i])
 end
 
-pred_save_te, errPerJoint_te, meanErrPerJoint_te = compute_distance(testset, nJoints)
-pred_save_tr, errPerJoint_tr, meanErrPerJoint_tr = compute_distance(trainset, nJoints)
+PCP = compute_PCP(testset)
+PCP = compute_PCP(trainset)
+pred_save_te, errPerJoint_te, meanErrPerJoint_te = compute_distance_joint(testset, nJoints)
+pred_save_tr, errPerJoint_tr, meanErrPerJoint_tr = compute_distance_joint(trainset, nJoints)
+avgMSE_te = compute_distance_MSE(testset)
+avgMSE_tr = compute_distance_MSE(trainset)
 
+print(string.format('PCP (test) :   %.2f(%%)', PCP))
+print(string.format('PCP (train):   %.2f(%%)', PCP))
+print(string.format('meanErrPerJoint (test) :   %.4f', meanErrPerJoint_te))
+print(string.format('meanErrPerJoint (train):   %.4f', meanErrPerJoint_tr))
+print(string.format('avgMSE (test) :   %.4f', avgMSE_te))
+print(string.format('avgMSE (train):   %.4f', avgMSE_tr))
 
--- *SAVE
--- save prediction results 
-save_prediction(pred_save_te, pred_save_tr, part, time)
+-- To check the results on images, save prediction outputs into .mat file
+matio.save(paths.concat(opt.save,string.format('pred_te_%s.mat', opt.t)), pred_save_te)
 
--- save log
-saveTable = {
-	['time'] = time,
-	['task'] = task,
-	['part'] = part,
-	['nJoints'] = nJoints,
-	['nTrainData'] = nTrainData,
-	['nTestData'] = nTestData,
-	['modelNumber'] = modelNumber,
-	['trainer.maxiteration'] = trainer.maxIteration,
-	['trainer.learningRate'] = trainer.learningRate,
-	['trainer.learningRateDecay'] = trainer.learningRateDecay,
-	['meanErrPerJoint_test'] = meanErrPerJoint_te,
-	['meanErrPerJoint_train'] = meanErrPerJoint_tr
-}
-print(saveTable)
-save_log(saveTable, time)
 
 
 

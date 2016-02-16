7d96bf9558cd55a6555d9bd42b2d2ab554c2c9b5
Namhoon Lee <namhoonlee09@gmail.com>
2016-02-16 15:44:17 -0500

    no clearState()

--- src/train.lua
+++ src/train.lua
@@ -85,7 +85,7 @@ function train()
 	end
 	--sanitize(model)
 	if epoch % 10 == 0 then
-		model:clearState()
+		--model:clearState()
 		saveDataParallel(paths.concat(opt.save, opt.t.. '_model_' .. epoch .. '.t7'), model)
 		torch.save(paths.concat(opt.save, opt.t.. '_optimState_' .. epoch .. '.t7'), optimState)
 	end

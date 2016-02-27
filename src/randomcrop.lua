6e553d496014830d20f99b724493a73e614a25eb
Namhoon Lee <namhoonlee09@gmail.com>
2016-02-27 17:18:23 -0500

    randomcrop updated

--- src/randomcrop.lua
+++ src/randomcrop.lua
@@ -10,7 +10,6 @@ function crop_label(img_ori, label_ori)
 	local bh_outer = bw_outer*2
 	local bw_crop = 64
 	local bh_crop = 128
-	local marginRatio = 0.2
 
 	-- compute bw_tight, bh_tight
 	local label_res = torch.reshape(label_ori, 14, 2)
@@ -43,36 +42,21 @@ function crop_label(img_ori, label_ori)
 	--print(lt_crop_x_min, lt_crop_y_min)
 	--print(lt_crop_x_max, lt_crop_y_max)
 	
-	if lt_crop_x_min + 64 > bw_outer then
-		lt_crop_x_min = bw_outer-64
+	if math.floor(lt_crop_x_min) <= 0 then
+		lt_crop_x_min = 1
 	end
-	
-	if lt_crop_x_max > lt_tight_x then
-		lt_crop_x_max = lt_tight_x
-	end
-	if lt_crop_y_max > lt_tight_y then
-		lt_crop_y_max = lt_tight_y
-	end
-	
-	-- post processing
-	if lt_crop_x_max + 64 > bw_outer then
-		lt_crop_x_max = bw_outer - 64
-	end
-	if lt_crop_y_max + 128 > bh_outer then
-		lt_crop_y_max = bh_outer - 128
+	if math.ceil(lt_crop_x_max)+ 64 >= bw_outer then
+		lt_crop_x_max = bw_outer-64-1
 	end
 
 	-- randomly select it
-	local lt_x_sel = math.ceil(torch.uniform(lt_crop_x_min+1, lt_crop_x_max-1))
-	local lt_y_sel = math.ceil(torch.uniform(lt_crop_y_min+1, lt_crop_y_max-1))
+	local lt_x_sel = math.ceil(torch.uniform(lt_crop_x_min, lt_crop_x_max))
+	local lt_y_sel = math.ceil(torch.uniform(lt_crop_y_min, lt_crop_y_max))
 
 	-- local image, resize and crop
 	local img_out = image.scale(img_ori, bw_outer .. 'x' .. bh_outer)
-	--print(lt_x_sel, lt_x_sel+bw_crop)
-	--print(lt_y_sel, lt_y_sel+bh_crop)
 	img_out = image.crop(img_out, lt_x_sel, lt_y_sel, lt_x_sel+bw_crop, lt_y_sel+bh_crop)
 	assert(img_out:size(2) == bh_crop and img_out:size(3) == bw_crop)
-
 
 	-- transform label
 	local scalar = torch.repeatTensor(torch.Tensor({bw_outer, bh_outer}), 14, 1)
@@ -92,8 +76,6 @@ function crop_label(img_ori, label_ori)
 	end
 
 	return img_out, label_out
-	--]]
-	
 end
 
 
@@ -106,6 +88,16 @@ function randomcrop(dataset)
 	local imagetensor = torch.Tensor(nData, 3, 128, 64)
 	local labeltensor = torch.Tensor(nData, 28)
 
+	--[[  Debugging purpose
+	local tmptensor = torch.Tensor(nData,1)
+	for i=1,nData do
+		tmptensor[i] = crop_label(dataset.data[i], dataset.label[i])
+	end
+	print(torch.min(tmptensor))
+	print(torch.max(tmptensor))
+	adf=adf+1
+	--]]
+	
 	for i=1,nData do
 		local crop, label = crop_label(dataset.data[i], dataset.label[i])
 		imagetensor[i] = crop
